b083519b060d2064fd80fdb26e1b56bfc73cd118
osama <osamanaim@gmail.com>
2021-01-20 19:54:56 -0500

    added sg ingress to open flask port

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "flask port"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
